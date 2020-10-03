b306f24403d972a6485a357fc77627ff68097bf1
michaelprimo <magiclegendstaff@gmail.com>
2020-10-03 23:11:13 +0200

    assets

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ self.addEventListener("install", event => {
         caches
             .open(cache_name)
             .then(cache => {
-                return cache.addAll(assets);
+                return cache.addAll(ASSETS);
             })
             .catch(err => console.log(err))
     );
