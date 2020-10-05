4283bdf16f5607c6a6961d26db90bc7e7e6fb316
michaelprimo <magiclegendstaff@gmail.com>
2020-10-05 11:54:56 +0200

    sw8

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+console.log('Hello from service-worker.js');
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
