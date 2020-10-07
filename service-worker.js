e98427c5a280e82aea4504d8e5f87e60bbc7a34d
michaelprimo <magiclegendstaff@gmail.com>
2020-10-07 16:39:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+/*
 console.log('Hello from service-worker.js');
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
@@ -7,3 +8,23 @@ if (workbox) {
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+*/
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+
+self.addEventListener('install', (event) => {
+  console.log('Inside the install handler:', event);
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Inside the activate handler:', event);
+});
+
+self.addEventListener(fetch, (event) => {
+  console.log('Inside the fetch handler:', event);
+});
