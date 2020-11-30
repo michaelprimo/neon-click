1ca232024db7cbd2dd7e4b38e3c2a472604df44a
michaelprimo <magiclegendstaff@gmail.com>
2020-11-30 19:03:22 +0100

    Service Worker edited

--- sw.js
+++ sw.js
@@ -1,62 +1,44 @@
-// sw.js
-
-let cache_name = "NeonCity"; // The string used to identify our cache
-
-self.addEventListener("install", event => {
-    console.log("installing...");
-    event.waitUntil(
-        caches
-            .open(cache_name)
-            .then(cache => {
-                return cache.addAll(assets);
-            })
-            .catch(err => console.log(err))
-    );
+const version = "1.0.0";
+const cacheName = `NeonClick-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/offline.html`,
+        `/index.css`,
+        `/colorAnimation.css`,
+        `/package.json`,
+        `/manifest.json`,
+        `/manifest.webmanifest`,
+        `/js/buttons.js`,
+        `/js/animations.js`,
+        `/js/check.js`,
+        `/js/countdown.js`,
+        `/js/generate.js`,
+        `/js/index.js`,
+        `/js/reset.js`,
+        `/js/state.js`,
+        `/AUTOMANI.TTF`,
+        `/Neon.ttf`,
+        `/SIMPLIFICA.ttf`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
 });
 
-const ASSETS = [
-    "/js/animations.js",
-    "/js/buttons.js",
-    "/js/check.js",
-    "/js/countdown.js",
-    "/js/generate.js",
-    "/js/index.js",
-    "/js/reset.js",
-    "/js/state.js",
-    "/AUTOMANI.ttf",
-    "/colorAnimation.css",
-    "/index.css",
-    "/index.html",
-    "/SIMPLIFICA.ttf",
-    "/images/icons-192.png",
-    "/images/icons-512.png",
-    "/images/pause.png",
-    "/images/reset.png",
-    "/offline.html",
-    "/manifest.webmanifest"
-];
-
-
-// sw.js
-/*
-self.addEventListener("fetch", event => {
-    console.log("You fetched " + event.url);
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
-*/
 
-self.addEventListener("fetch", event => {
-    if (event.request.url === "https://www.simicart.com/") {
-        // or whatever your app's URL is
-        event.respondWith(
-            fetch(event.request).catch(err =>
-                self.cache.open(cache_name).then(cache => cache.match("/offline.html"))
-            )
-        );
-    } else {
-        event.respondWith(
-            fetch(event.request).catch(err =>
-                caches.match(event.request).then(response => response)
-            )
-        );
-    }
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
